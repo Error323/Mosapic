4e227148a45e194804c262089c2891ad056a1159
Error323 <folkerthuizinga@gmail.com>
2011-06-21 07:23:33 +0200

    Add boost program_options

--- src/Hexapic.cpp
+++ src/Hexapic.cpp
@@ -2,42 +2,77 @@
 #include <string>
 #include <cstdlib>
 #include <sstream>
+#include <boost/program_options.hpp>
 
 #include "Types.hpp"
 #include "Version.hpp"
 #include "HexaCrawler.hpp"
 #include "HexaMosaic.hpp"
-#include "Image.hpp"
 
-void PrintUsage() {
-	std::cout << "Usage: hexapic" << std::endl;
-}
+namespace po = boost::program_options;
 
 int main(int argc,char **argv) {
-	std::cout << HUMAN_NAME << std::endl;
-	switch (argc)
-	{
-		case 3: {
-			cString source_dir = argv[1];
-			cString dest_dir   = argv[2];
-			HexaCrawler hc;
-			hc.Crawl(source_dir, dest_dir);
-		}
-		case 6: {
-			cString source_image = argv[1];
-			cString dest_image   = argv[2];
-			cString database     = argv[3];
-			cInt width           = atoi(argv[4]);
-			cInt height          = atoi(argv[5]);
-			HexaMosaic hm(source_image, dest_image, database, width, height);
-			hm.Create();
-			break;
-		}
+	po::options_description generic("Generic options");
+	generic.add_options()
+		("version,v", "print version string")
+		("help,h", "produce help message")
+	;
 
-		default: {
-			PrintUsage();
-			exit(1);
-		}
+	po::options_description crawl("Crawler options");
+	crawl.add_options()
+		("image-dir,i", po::value<String>(), "image directory")
+		("output-dir,o", po::value<String>(), "cache directory and database")
+	;
+
+	po::options_description hexapic("Hexapic options");
+	hexapic.add_options()
+		("input-image", po::value<String>(), "source image")
+		("output-image", po::value<String>(), "output image")
+		("database", po::value<String>(), "database directory")
+		("width", po::value<String>(), "width in tile size")
+		("height", po::value<String>(), "height in tile size")
+	;
+
+	po::options_description cmdline_options;
+	cmdline_options.add(generic).add(crawl).add(hexapic);
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, cmdline_options), vm);
+	po::notify(vm);
+
+	if (vm.count("version"))
+	{
+		std::cout << HUMAN_NAME << std::endl;
 	}
+	else
+	if (vm.count("image-dir") && vm.count("output-dir"))
+	{
+		cString image_dir = vm["image-dir"].as<String>();
+		cString output_dir   = vm["output-dir"].as<String>();
+
+		HexaCrawler hc;
+		hc.Crawl(image_dir, output_dir);
+	}
+	else
+	if (vm.count("input-image") && vm.count("output-image") && 
+		vm.count("database") && vm.count("width") && vm.count("height"))
+	{
+		cString input_image  = vm["input-image"].as<String>();
+		cString output_image = vm["output-image"].as<String>();
+		cString database     = vm["database"].as<String>();
+		cInt width           = vm["width"].as<int>();
+		cInt height          = vm["height"].as<int>();
+
+		HexaMosaic hm(input_image, output_image, database, width, height);
+		hm.Create();
+	}
+	else
+	{
+		std::cout << std::endl << generic << std::endl;
+		std::cout << std::endl << crawl << std::endl;
+		std::cout << std::endl << hexapic << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
