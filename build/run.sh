76d23bb15bb7f0156e559455421297e688c37df6
Error323 <folkerthuizinga@gmail.com>
2011-12-13 00:53:03 +0100

    Added batch script

---
+++ build/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+INPUTDIR=$1
+DATABASE=$2
+WIDTH=18
+HEIGHT=15
+DIM=16
+RAD=12
+
+for IMG in ${INPUTDIR}*
+do
+    cmd="./hexapic --input-image=${IMG} --database=${DATABASE} --width=${WIDTH} --height=${HEIGHT} --dimensions=${DIM} --min-radius=${RAD}"
+
+    echo ""
+    echo ${cmd}
+    ${cmd}
+done
+
+#zip tessa.zip *.jpg
