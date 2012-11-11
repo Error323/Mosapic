ab3dfe670d084b79aa32729e9f121caa11b25e8a
Error323 <folkerthuizinga@gmail.com>
2012-11-11 14:00:58 +0100

    Update batch-start.sh

--- scripts/batch-start.sh
+++ scripts/batch-start.sh
@@ -2,14 +2,14 @@
 
 INPUTDIR=$1
 DATABASE=$2
-WIDTH=30
+WIDTH=20
 HEIGHT=18
 DIM=16
 RAD=12
 
 for IMG in ${INPUTDIR}*
 do
-    cmd="./Hexapic --input-image=${IMG} --database=${DATABASE} --width=${WIDTH} --height=${HEIGHT} --dimensions=${DIM} --min-radius=${RAD}"
+    cmd="./../build/Hexapic --input-image=${IMG} --database=${DATABASE} --width=${WIDTH} --height=${HEIGHT} --dimensions=${DIM} --min-radius=${RAD}"
 
     echo ""
     echo ${cmd}
